5ea40ab92b84bdea672ed3b06609c4a567ceadee
LaykenV <laykenv@gmail.com>
2022-05-18 11:38:52 -0500

    rendering cars. taking a break

--- src/components/shop.js
+++ src/components/shop.js
@@ -1,9 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import cars from './carsData';
 
 function Shop() {
     return(
         <div className='shopContainer'>
-            shop
+           {cars.map((car) => {
+               return (
+                   <div className='carContainer' key={car.id}>
+                       <img className='carImage' src={car.image} alt='car'></img>
+                       <h3 className='carName'>{car.name}</h3>
+                       <p className='carDescription'>{car.description}</p>
+                       <div className='addToCartDiv'>
+                           <button className='addToCartButton'>Add To Cart</button>
+                           <div className='addToCartAmount'>
+                            <button className='addToCartAmountButton'>-</button>
+                            <span>1</span>
+                            <button className='addToCartButton'>+</button>
+                           </div>
+                       </div>
+                       <div className='line'></div>
+                   </div>
+               )
+           })}
         </div>
     )
 }
